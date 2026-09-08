c4205d626a5b3fe876cb7a0a1385ae7c61b0f02d
ug12-lab <ulasgunduz12@gmail.com>
2026-09-09 00:03:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -210,6 +210,11 @@ def ana_sayfa():
     return FileResponse("taslak.html")
 
 
+@app.get("/logo.png")
+def logo():
+    return FileResponse("logo.png")
+
+
 @app.get("/arama")
 def arama_yap(q: str):
     sonuclar = []
